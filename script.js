aae79967b4cbbc6c5818870d0d19a6fb884ffc1d
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-05-26 18:52:32 +0200

    changed: picture working on load - check if classify is working correct with phone

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-let net;
+const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+let modelHasLoaded = false;
+
+function modelLoaded() {
+  console.log('Yay, the model has loaded!');
+  document.querySelector(".container h1").innerText = "Model loaded. Click capture to take a photo.";
+  modelHasLoaded = true;
+}
+
+const webcamElement = document.getElementById('webcam');
 const captureButton = document.getElementById('capture-btn');
 const capturedImageElement = document.getElementById('captured-img');
 const resultElement = document.querySelector('.classify-info');
 
-
-const webcamElement = document.getElementById('webcam');
 const canvasElement = document.getElementById('canvas');
 const snapSoundElement = document.getElementById('snapSound');
 const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
 
-async function setup() {
-  console.log('Loading MobileNet model...');
-  net = await mobilenet.load();
-  console.log('Model loaded.');
-  document.querySelector("h1").innerText = "Model loaded. Click capture to take a photo.";
-}
-
-captureButton.addEventListener('click', async () => {
-  const picture = webcam.snap();
-  capturedImageElement.src = picture;
-  resultElement.innerHTML = '<h2>Classifying...</h2>';
-  const img = new Image();
-  img.src = picture;
-  img.onload = async () => {
-    const predictions = await net.classify(img);
-    displayPredictions(predictions);
-  };
+captureButton.addEventListener('click', () => {
+  if (modelHasLoaded) {
+    const picture = webcam.snap();
+    capturedImageElement.src = picture;
+    resultElement.innerHTML = '<h2>Classifying...</h2>';
+    const img = new Image();
+    img.src = picture;
+    img.onload = () => {
+      classifier.classify(img, (err, results) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        displayPredictions(results);
+      });
+    };
+  }
 });
 
 function displayPredictions(predictions) {
   resultElement.innerHTML = '';
-  predictions.forEach(prediction => {
-    const p = document.createElement('p');
-    p.innerText = `${prediction.className}: ${(prediction.probability * 100).toFixed(2)}%`;
-    resultElement.appendChild(p);
-  });
+  for (let result of predictions) {
+    console.log(result);
+    if (result.confidence >= 0.1) {
+      resultElement.innerHTML += `<h2>${result.label}: ${(result.confidence * 100).toFixed(2)}%</h2>`;
+    }
+  }
+  if (resultElement.innerHTML === '') {
+    resultElement.innerHTML = '<h2>No result found</h2>';
+  }
 }
 
 webcam.start()
-  .then(result => {
+  .then(() => {
     console.log('Webcam started');
-    setup();
   })
   .catch(err => {
     console.error(err);
